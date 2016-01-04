f096bd3102b0109b9de1072959d299ba01657a9a
drewotteson <drew.otteson@yahoo.com>
2016-01-04 15:22:51 -0600

    overtime feature

--- ex1.js
+++ ex1.js
@@ -2,8 +2,29 @@
 var hoursworked;
 var rateperhour;
 var totalpay;
-hoursworked = 37
-rateperhour = 22.50
-totalpay = hoursworked * rateperhour;
+var overtimehours;
+var overallpay;
+var hourcalc;
+var overallcalc;
 
-console.log(totalpay)
+hoursworked = 47
+rateperhour = 23.43
+totalpay = hoursworked * rateperhour;
+overtimehours = hoursworked - 40;
+overallpay = overtimehours * rateperhour * 1.5;
+hourcalc = hoursworked - overtimehours;
+overallcalc = hourcalc * rateperhour + overallpay;
+if (hoursworked >= 40) {
+    console.log(Math.round(overallcalc * 100)/100);
+}else { (hoursworked <= 40) 
+    console.log(totalpay);
+}
+
+
+
+
+
+
+
+
+
